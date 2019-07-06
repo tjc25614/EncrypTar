c6155cae0fe057c14e6b11de7ceb0367ee5119b7
Tom Conroy <tjc25614@outlook.com>
2019-07-06 15:59:49 -0400

    Allowed specifying multiple files/directories to be archived at once

--- EncrypTar.py
+++ EncrypTar.py
@@ -16,18 +16,19 @@ TAG_LEN = 16
 
 MAGIC_NUMBER = b'\x25\x24'
 
-def CreateTar(files, recursive):
+def CreateTar(files, recursive, current_directory):
     tar_buffer = io.BytesIO()
-    if os.path.isfile(files):
-        directory, filename = os.path.split(files)
-        if directory != '':
-            os.chdir(directory) # make all file backups relative to the source directory
-        tar_file = tar.open(mode='w:xz', fileobj=tar_buffer) # write an lmza-compressed tarfile to the buffer
-        tar_file.add(filename, recursive=recursive)
-    else:
-        os.chdir(files)
-        tar_file = tar.open(mode='w:xz', fileobj=tar_buffer)
-        tar_file.add('.', recursive=recursive)
+    tar_file = tar.open(mode='w:xz', fileobj=tar_buffer) # write an lmza-compressed tarfile to the buffer
+    for file in files:
+        os.chdir(current_directory)
+        if os.path.isfile(file):
+            directory, filename = os.path.split(file)
+            if directory != '':
+                os.chdir(directory) # make all files placed relative to the source directory
+            tar_file.add(filename, recursive=recursive)
+        else:
+            os.chdir(file)
+            tar_file.add('.', recursive=recursive)
 
     tar_file.close()
     tar_data = tar_buffer.getvalue()
@@ -65,20 +66,22 @@ def WriteArchive(archive_filename, salt, nonce, enc_archive, tag):
 def RunEncrypTar():
     # setup commandline argument parsing using argparse module
     parser = argparse.ArgumentParser(description='Python script that does encrypted archives of both files and directories.')
-    parser.add_argument("FILE", help='The file/directory to archive/(extract to), not recursive by default')
-    parser.add_argument("ARCHIVE", help='Name of archive file to create/restore')
-    parser.add_argument('-p', '--passphrase', help='Optional file containing passphrase to use for encryption')
+    parser.add_argument("ARCHIVE", help='Name of archive file to create/restore.')
+    parser.add_argument("FILES", nargs='*', default=['.'], help='The file/directory to archive/(extract to). Not recursive by default. Defaults to current working directory.')
+    parser.add_argument('-p', '--passphrase', help='Optional file containing passphrase to use for encryption.')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-a', '--archive', action='store_true', help='Run in archive mode, default mode')
-    group.add_argument('-e', '--extract', action='store_true', help='Run in extract mode')
-    parser.add_argument('-R', '--recursive', action='store_true', help='Encrypt directories recursively')
+    group.add_argument('-a', '--archive', action='store_true', help='Run in archive mode, default mode.')
+    group.add_argument('-e', '--extract', action='store_true', help='Run in extract mode.')
+    parser.add_argument('-r', '--recursive', action='store_true', help='Encrypt directories recursively.')
     args = parser.parse_args()
-
+    
     current_directory = os.getcwd()
 
     archive = True
     if args.extract:
         archive = False
+        if len(args.FILES) > 1:
+            raise ValueError("Please specify only one extract directory.")
 
     if args.passphrase:
         with open(args.passphrase, 'r') as passphrase_file:
@@ -91,19 +94,20 @@ def RunEncrypTar():
         recursive = True
 
     if archive:
-        if not os.path.isfile(args.FILE) and not os.path.isdir(args.FILE):
-            raise ValueError("The file/directory must exist for it to be archived!")
+        for file in args.FILES:
+            if not os.path.isfile(file) and not os.path.isdir(file):
+                raise ValueError("The file/directory " + file + " must exist for it to be archived!")
 
         salt = os.urandom(SALT_LEN) # get a salt for key derivation
         key = DeriveKey(passphrase, salt)
-        archive = CreateTar(args.FILE, recursive)
+        archive = CreateTar(args.FILES, recursive, current_directory)
         nonce = os.urandom(NONCE_LEN) # create a nonce for AES encryption
         enc_archive, tag = Encrypt(archive, key, nonce)
         os.chdir(current_directory)
         WriteArchive(args.ARCHIVE, salt, nonce, enc_archive, tag)
 
     else: # extract
-        if not os.path.isdir(args.FILE):
+        if not os.path.isdir(args.FILES[0]):
             raise ValueError("The extraction location must be a directory")
         archive_file = open(args.ARCHIVE, 'rb')
         if archive_file.read(2) != MAGIC_NUMBER:
@@ -114,7 +118,7 @@ def RunEncrypTar():
         tag = archive_file.read(TAG_LEN)
         enc_archive = archive_file.read()
         archive = Decrypt(enc_archive, key, nonce, tag)
-        RestoreTar(archive, args.FILE)
+        RestoreTar(archive, args.FILES[0])
         archive_file.close()
 
 if __name__ == "__main__":
